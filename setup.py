ebf4596ba0340cf276520e23c086dec6f267036e
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-11-29 14:57:25 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="xmpdf",
+      version="0.1",
+      description="A Python module for extracting email metadata and text from \
+a PDF file",
+      author="Ben Lis",
+      url="https://github.com/history-lab/xmpdf",
+      license="Apache License",
+      py_modules=["xmpdf"])
