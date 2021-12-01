5ac8054ad9a84d6ad02f4ace47f0019d887addc1
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-12-01 16:01:20 -0500

    Added jsonpickle to install requirements.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name="xmpdf",
-      version="0.1",
-      description="A Python module for extracting email metadata and text from \
+      version="0.2",
+      description="A Python package for extracting email metadata and text from \
 a PDF file",
       author="Ben Lis",
       url="https://github.com/history-lab/xmpdf",
-      license="Apache License",
-      install_requires=["pdftotext", "python-magic"],
+      license="MIT License",
+      install_requires=["pdftotext", "python-magic", "jsonpickle"],
       py_modules=["pgparse", "xmpdf"])
