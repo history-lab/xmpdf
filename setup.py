6197202cbf4c9735a7e3ca030fc4991fe14451fa
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-02-01 15:04:21 -0500

    Added History Lab @ Columbia University to author field

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name="xmpdf",
       version="0.3",
       description="A Python package for extracting email metadata and text from \
 a PDF file",
-      author="Ben Lis",
+      author="Ben Lis, History Lab @ Columbia University",
       url="https://github.com/history-lab/xmpdf",
       license="MIT License",
       install_requires=["pdftotext", "python-magic", "jsonpickle"],
