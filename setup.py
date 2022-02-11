583370798d8cb81791563dba12f30576d40cf25f
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-02-11 18:38:21 -0500

    Incremented version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_long_description():
 
 
 setup(name="xmpdf",
-      version="0.4",
+      version="0.4.1",
       long_description=get_long_description(),
       long_description_content_type="text/markdown",
       description="Extracts email metadata and text from a PDF file",
